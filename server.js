f8a964e59b88d2b8b07ccf539275a7be2db318bc
curciros <38756666+curciros@users.noreply.github.com>
2021-05-21 19:04:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Excercise12 new change Stefan');
+    res.send('Demo DevOps Präsi');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
