10aba8e67f4e38334ff812a8937f4fac6a34a7fe
curciros <38756666+curciros@users.noreply.github.com>
2021-05-21 19:11:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Juhu es funktioniert');
+    res.send('Jetzt ändern wir etwas und es soll direkt wieder zum Testen zur Verfügung stehen');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
